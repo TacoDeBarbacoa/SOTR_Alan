dbfdbb36819e34eae7f129ab7127a486f0fb0300
TacoDeBarbacoa <114273007+TacoDeBarbacoa@users.noreply.github.com>
2022-11-16 15:06:47 -0600

    Create queue.ino

---
+++ Actividad 04/queue.ino
@@ -0,0 +1,71 @@
+// Use only core 1 for demo purposes
+#if CONFIG_FREERTOS_UNICORE
+  static const BaseType_t app_cpu = 0;
+#else
+  static const BaseType_t app_cpu = 1;
+#endif
+
+// Settings
+static const uint8_t msg_queue_len = 5;
+
+// Globals
+static QueueHandle_t msg_queue;
+
+//*****************************************************************************
+// Tasks
+
+void printMessages(void *parameters) {
+
+  int item;
+
+  // Loop forever
+  while (1) {
+
+    
+    if (xQueueReceive(msg_queue, (void *)&item, 0) == pdTRUE) {
+    }
+    Serial.println(item);
+
+    // Wait before trying again
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
+
+// Main 
+
+void setup() {
+
+  // Configure Serial
+  Serial.begin(115200);
+
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  Serial.println();
+  Serial.println("---FreeRTOS Queue Demo---");
+
+  msg_queue = xQueueCreate(msg_queue_len, sizeof(int));
+
+  // Start print task
+  xTaskCreatePinnedToCore(printMessages,
+                          "Print Messages",
+                          1024,
+                          NULL,
+                          1,
+                          NULL,
+                          app_cpu);
+}
+
+void loop() {
+
+  static int num  = 0;
+
+  if (xQueueSend(msg_queue, (void *)&num, 10) != pdTRUE) {
+    Serial.println("Queue full");
+  }
+  num++;
+
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
+/* El comportamiento del queue depende totalmente  en el tiempo de espera de las tareas 
+y el tiempo para refrescarse ya que si disminuimos el tiempo de este ultimo enviará 
+valores hasta el maximo de capacidad y si este lo aumentamos pues tardara en refrescar el valor
+por lo que imprimirá el mismo valor hasta que se vuelva a refrescar*/
